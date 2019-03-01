7ad4e57b32accfbbf913888b0b20098d9e456a61
Perez Quintero <000402095@bga.upb>
2019-03-01 12:05:31 -0500

    Ejercicio resuelto

--- mayor.py.py
+++ mayor.py.py
@@ -1,3 +1,27 @@
-a=int(input("Digite un número"))
-if a>0:
-  print("Su número es mayor a 0")
+n=int(input("De 10 números cuantos son positivos, negativos y cero"))
+
+pos=0
+
+neg=0
+
+cero=0
+
+conta=0
+
+if n>0:
+  
+  pos=pos+1
+if n<0:
+  
+  neg=neg+1
+
+if n=0:
+  
+cero=cero+1
+
+while (conta <=10)
+
+
+
+print("Hay {} números positivos, hay {} números negativos y hay {} ceros".format(pos,neg,cero))
+ 
